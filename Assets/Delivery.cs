4e50d6bee699b9ab73597976f07798f6bfd45d79
suchorsky <50916690+suchorsky@users.noreply.github.com>
2022-04-04 22:02:13 +0200

    Main logic created

---
+++ Assets/Delivery.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+  [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+  [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+  [SerializeField] float destroyDelay = 0.5f;
+  bool hasPackage;
+
+  SpriteRenderer spriteRenderer;
+
+  private void Start()
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    Debug.Log("Bang!");
+  }
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Package" && !hasPackage)
+    {
+      hasPackage = true;
+      spriteRenderer.color = hasPackageColor;
+      Destroy(other.gameObject, destroyDelay);
+      Debug.Log("Package picked up");
+    }
+    else if (other.tag == "Customer" && hasPackage)
+    {
+      hasPackage = false;
+      spriteRenderer.color = noPackageColor;
+      Debug.Log("Delivered to customer");
+    }
+  }
+}
